f47f46ea6ae7d64f593c8b3a0be74a5acfde0e73
Tálas László Martin <talasl21@ADY-NAGYATAD.SULINET.HU>
2022-09-09 13:55:52 +0200

    Javitott

--- elso.py
+++ elso.py
@@ -1,2 +1,5 @@
-print("Hello, World!",end="\n-------------------\n")
+print("Hello, World!",end="/n-------------------/n")
 print("Hello, world!", "Hello világ",42,sep=" --- ")
+
+
+C:/Users/Public/Programs/mblock/resources/app/mlink-v2/exec/python-env/win/python.exe
