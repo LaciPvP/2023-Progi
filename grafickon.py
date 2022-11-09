ce2f760906ee75872b140c9327704e715168b5b8
Tálas László Martin <talasl21@ADY-NAGYATAD.SULINET.HU>
2022-11-09 14:04:22 +0100

    készmat

--- grafickon.py
+++ grafickon.py
@@ -1,5 +1,20 @@
 import matplotlib.pyplot as plt
 
+def fv(a,b,c):
+    xPont=[ ] 
+    yPont=[ ]
+
+    for x in range(-10,10):
+        xPont.append(x)
+        yPont.append(a*x**2+b*x+c)
+
+        return(xPont , yPont)
+
+def fv2(*egyutthatok):
+    for i in egyutthatok:
+        
+
+import numpy as np
 
 x=[1,10]
 y=[10,1]
@@ -27,4 +42,14 @@ for x in range(-10,10):
     yPont.append(16*x**2 -3 +13)
 
 plt.plot(xPont,yPont)
-plt.show()
+
+pontok=fv(10,100,1)
+plt.plot(pontok[0],pontok[1])
+
+#plt.show()
+
+fv2(1,2,3,4,5,)
+
+
+
+
