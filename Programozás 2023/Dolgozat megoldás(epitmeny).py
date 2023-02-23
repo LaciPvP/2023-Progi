ed9a1b22a19e143ee4b147ccd7a068921b87263a
Tálas László Martin <talasl21@ADY-NAGYATAD.SULINET.HU>
2023-02-23 13:20:13 +0100

    Kész 02.23

--- Programozás 2023/Dolgozat megoldás(epitmeny).py
+++ Programozás 2023/Dolgozat megoldás(epitmeny).py
@@ -1,17 +1,16 @@
 def ado(adosav,alapterulet):
+    alapterulet=int(alapterulet)
     if adosav == "A":
-        ar= int(arak[0])*alapterulet
+        ar=int(arak[0])*alapterulet
     elif adosav == "B":
-        ar= int(arak[1])*alapterulet
+        ar=int(arak[1])*alapterulet
     else:
-         ar= int(arak[2])*alapterulet
+         ar=int(arak[2])*alapterulet
 
     if ar>=10000:
         return ar
     else:
-        return 0
-
-
+        return 0 
 
 
 
@@ -47,7 +46,29 @@ else:
     for e in cimek:
         print("{} utca {}".format(e[1],e[2]))
 
-print(ado("C",120))
+#print(ado("C",120))
+
+        
+hazakA=[e for e in hazak if e[3]=="A"]
+
+hazakB=[]
+for e in hazak:
+    if e[3]==adoszam:
+        hazakB.append(e)
+
+hazakC=[e for e in hazak if e[3]=="C"]
+
+arak=[]
+for i in range(len(hazakA)):
+    hazakA[i].append(ado(hazakA[i][3],hazakA[i][4]))
+
+for i in range(len(hazakA)):
+    hazakB[i].append(ado(hazakB[i][3],hazakB[i][4]))
+
+for i in range(len(hazakA)):
+    hazakC[i].append(ado(hazakC[i][3],hazakC[i][4]))
+
+
     
 
 
