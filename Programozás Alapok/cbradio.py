81403d79fa0d42a9c3f00896e6f11bf0210364ba
LaciPvP <talasl21@ADY-NAGYATAD.SULINET.HU>
2023-05-31 13:13:18 +0200

    ma semmi nemvolt

---
+++ Programozás Alapok/cbradio.py
@@ -0,0 +1,53 @@
+class radio:
+    def __init__(self):
+        self.bejegyzes
+        
+print("2. Feladat")
+f=open("cb.txt")
+ureslist=[]
+for e in f:
+    ureslist.append(e.strip())
+f.close()
+print(ureslist)
+
+
+szamolAS=len(ureslist)
+print("3. Feladat, az állományban ennyi a bejegyzések száma: {0}".format(szamolAS))
+
+print("4. Feladat")
+    def negyes_valto(self):
+print("5.Feladat")
+    def Nev_beker(self,nev,hivszam):
+        self.nev=nev
+        self.hivszam=hivszam
+        beker=input("Kérek egy nevet:")
+        if beker in ureslist:
+                print("Ennyiszer használta a Cb rádiót:")
+
+print("6. Feladat")
+    def AtszamolPercre(self,ora,perc):
+        self.ora=oraszam
+        self.perc=percszam
+        ora=int(input("Kérek egy óraszámot:"))
+        perc=int(input("Kérek egy percszámot:"))
+
+
+    
+    
+        
+
+
+    
+
+
+          
+
+   
+
+
+
+    
+
+    
+            
+        
