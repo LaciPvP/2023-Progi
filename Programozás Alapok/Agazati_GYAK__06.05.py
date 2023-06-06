6f6d06be96d77601794b5f0fe2778d5eed2eea57
Tálas László Martin <talasl21@ADY-NAGYATAD.SULINET.HU>
2023-06-06 11:29:58 +0200

    kesz 06.06

--- Programozás Alapok/Agazati_GYAK__06.05.py
+++ Programozás Alapok/Agazati_GYAK__06.05.py
@@ -1,11 +1,44 @@
+import random
 import modulka
 
 f=open("szavak.txt")
-sorok=[]
+l=[]
 for e in f:
-    sorok.append(e.split())
-    
-
+    l.append(e.replace("\"",'').strip())
 
 f.close()
-print(sorok)
+#--------------------------------
+sorok=l[0].split(", ")
+
+szavak=[]
+for e in sorok:
+    szavak.append(modulka.szavak(e))
+
+print(szavak)
+
+print("2.Feladat")
+rejtett=random.choice(szavak)
+print(rejtett.szavak)
+#--------------------------------
+print("3.Feladat")
+tippek=[]
+while True:
+    be=input("Kérek egy szót(min/max 6 betű): ")
+    tippek.append(be)
+    
+    if be=="stop":
+        break
+
+    vissza=rejtett.minta(be)
+    
+    print("Az eredmény: {}".format(vissza))
+    if vissza==be:
+        break
+
+if tippek[-1]=="stop":
+    pass
+else:
+    print("{} tippeléssel sikerült kitalálni.".format(len(tippek)))    
+
+        
+    
--- Programozás Alapok/modulka.py
+++ Programozás Alapok/modulka.py
@@ -1,3 +1,12 @@
 class szavak:
     def __init__(self,szavak):
         self.szavak=szavak
+    def minta(self,szo):
+        vissza=""
+        for i in range(6):
+            if szo[i]==self.szavak[i]:
+                vissza+=szo[i]
+            else:
+                vissza+="."
+
+        return vissza
